6752e72ea03365ebbd5f611ed973e383b112d7e7
David Mecalco Alejo <dreivixmecal@gmail.com>
2025-09-23 12:19:37 -0600

    Agregamos el archivo slicing de Strings

--- slicing.py
+++ slicing.py
@@ -17,3 +17,25 @@ print(text[::-1])
 text = 'Anita lava la tina'
 print(text)
 print(text[::-1])
+
+#Otros ejemplos para el Slicing de Strings
+text = 'Hola Mundo'
+print(text[0:4])
+print(text[:4])
+print(text[2:])
+print(text[::3])
+
+text = 'Hola Mundo'
+new_text = text[:5] + text[5:].replace('Hola','Python')
+print(new_text)
+
+text = 'Python es genial'
+parts = text.split()
+parts2 = parts[:2]
+print(parts)
+print(parts2)
+parts_reverse = parts[::-1]
+print(parts_reverse)
+
+text_reverse = ' '.join(parts_reverse)
+print(text_reverse)
